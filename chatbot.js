96555dfaa2d05d70d3b205b4e87e7202f7ed4632
Miguel Ceballos <ceballosmiguel777@gmail.com>
2016-09-28 14:50:35 -0700

    ayyy

--- chatbot.js
+++ chatbot.js
@@ -1,14 +1,23 @@
 var responses = {
 	"Hi": "Hello my name is Eliza",
 	"How old are you": "2 weeks old",
-  "Whats your favorite color" : "raibow"
+  "Whats your favorite color" : "raibow",
+  "ayyy" : "ayyyy"
 }
 
+var fallBack = ["Que?", "Can't hear you!", ""];
 function speak() {
 
   var userInput = $("#input").val();
   var response = responses[userInput];
 
-  $('#chat-area').prepend(response + "</br>");
+  if (response === undefined){
+      $('#chat-area').prepend(fallBack[0] + "...");
+  }
+
+  else {
+      $('#chat-area').prepend(response + "</br>");
+  }
   $('#chat-area').prepend(userInput + "</br>");
+
 }
