683de56c8576491637d9c2d7d9bac5cccb6e4aad
Miguel Ceballos <ceballosmiguel777@gmail.com>
2016-09-30 14:51:26 -0700

    finished :)

--- chatbot.js
+++ chatbot.js
@@ -1,23 +1,34 @@
+var now = new Date(Date.now());
+
 var responses = {
-	"Hi": "Hello my name is Eliza",
-	"How old are you": "2 weeks old",
-  "Whats your favorite color" : "raibow",
-  "ayyy" : "ayyyy"
+	"what is your name?": "Hello my name is Eliza",
+	"how old are you?": "2 weeks old",
+  "whats your favorite color?" : "raibow",
+  "ayyy" : "ayyyy",
+  "what time is it?" : now
 }
 
-var fallBack = ["Que?", "Can't hear you!", ""];
+
+var fallBack = ["Que?", "Can't hear you!"];
 function speak() {
 
   var userInput = $("#input").val();
+  userInput = userInput.toLowerCase();
+
   var response = responses[userInput];
 
   if (response === undefined){
-      $('#chat-area').prepend(fallBack[0] + "...");
+      $('#chat-area').prepend(fallBack[1] + "..." + "</br>");
   }
 
   else {
       $('#chat-area').prepend(response + "</br>");
   }
   $('#chat-area').prepend(userInput + "</br>");
-
 }
+
+  $(document).keyup(function(event){
+     if(event.keyCode == 13){
+         speak();
+     }
+});
